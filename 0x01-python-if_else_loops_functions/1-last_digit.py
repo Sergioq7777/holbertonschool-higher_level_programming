cbdc96992607c82937930363d8b713abde0b67b9
sergioq7777 <sergioq.7777@gmail.com>
2019-12-11 00:32:43 +0000

    [Fix]betty

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,12 +7,11 @@ if number > 0:
 else:
     modulo = number % -10
 
+    if modulo > 5:
+        print("last digit of {} is {} and is grater than 5".format(number, modulo))
 
-if modulo > 5:
-    print("last digit of {0} is {1} and is grater than 5".format(number, modulo))
+    elif modulo == 0:
+        print("Last digit of {} is {} and is 0".format(number, modulo))
 
-elif modulo == 0:
-    print("Last digit of {0} is {1} and is 0".format(number, modulo))
-
-elif modulo < 6 and number != 0:
-    print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, modulo))
+    elif modulo < 6 and number != 0:
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, modulo))
