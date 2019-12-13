5691614c328c6a4afd892a9742dc73b6ef1ebdb0
sergioq7777 <sergioq.7777@gmail.com>
2019-12-13 03:45:04 +0000

    change some par

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+    import calculator_1 as imp
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, imp.add(a, b)))
+    print("{} - {} = {}".format(a, b, imp.sub(a, b)))
+    print("{} * {} = {}".format(a, b, imp.mul(a, b)))
+    print("{} / {} = {}".format(a, b, imp.div(a, b)))
