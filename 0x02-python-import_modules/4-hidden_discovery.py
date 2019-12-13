220b506dcd9c26704877b39f88ea30e473564ae0
sergioq7777 <sergioq.7777@gmail.com>
2019-12-13 04:29:26 +0000

    add

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def main():
+    i = 0
+    for i in dir(hidden_4):
+        if i[0:1] != '_':
+            print("{}".format(i))
+if __name__ == '__main__':
+    import hidden_4
+    main()
