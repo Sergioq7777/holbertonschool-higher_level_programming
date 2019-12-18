e85a3c15a5d349b8c762e673ac02c56cf091faba
sergioq7777 <sergioq.7777@gmail.com>
2019-12-18 03:42:10 +0000

    Dont forget space

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for j in range(len(matrix)):
+        for i in range(len(matrix[j])):
+            if j == len(matrix[j]) - 1:
+                print("{}".format(matrix[j][i]), end=' ')
+            else:
+                print(matrix[j][i], end=' ')
+        print()
