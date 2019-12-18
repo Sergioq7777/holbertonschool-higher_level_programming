e129ac772803e10fd79b448dff4f04e78fdef41e
sergioq7777 <sergioq.7777@gmail.com>
2019-12-18 04:19:54 +0000

    ADD list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    lst = list.copy(my_list)
+    for i in range(len(my_list)):
+        if (my_list[i] % 2 == 0):
+            lst[i] = 1
+        elif (my_list[i] % 2 != 0):
+            lst[i] = 0
+    return(lst)
