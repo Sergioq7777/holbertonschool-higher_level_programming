72b76081b414899f30a7d92152908037e3505a4f
sergioq7777 <sergioq.7777@gmail.com>
2019-12-18 04:07:35 +0000

    append

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    s1 = list(tuple_a)
+    s2 = list(tuple_b)
+    s1.append(0)
+    s1.append(0)
+    s2.append(0)
+    s2.append(0)
+    s3 = [s1[0] + s2[0], s1[1] + s2[1]]
+    s3 = s3[:3]
+    return(tuple(s3))
