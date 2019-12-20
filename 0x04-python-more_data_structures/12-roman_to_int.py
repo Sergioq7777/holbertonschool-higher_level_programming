66b8fb71fad1d3a59fa88b5d90cacdcbd129eac1
sergioq7777 <sergioq.7777@gmail.com>
2019-12-20 04:48:01 +0000

    Romans

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    rmns = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if not roman_string:
+        return 0
+    elif not isinstance(roman_string, str):
+        return 0
+    elif roman_string in rmns:
+        return rmns.get(roman_string)
+    else:
+        num = rmns[roman_string[0]]
+        for count in range(len(roman_string) - 1):
+            future = rmns[roman_string[count + 1]]
+            present = rmns[roman_string[count]]
+            num += future if future <= present else +future - 2 * present
+        return num
