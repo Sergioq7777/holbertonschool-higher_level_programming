aec49740e5c92202b47f4c036478fb24ead5151e
sergioq7777 <sergioq.7777@gmail.com>
2020-01-08 18:04:46 -0500

    Format

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         for num in range(0, x):
-            print(my_list[num], end='')
+            print("{}".format(my_list[num]), end='')
             i += 1
         print()
         return i
