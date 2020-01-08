9155c046a18ed100e3cca6125a137b250d997878
sergioq7777 <sergioq.7777@gmail.com>
2020-01-08 16:24:55 -0500

    Pep

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+
+    i = 0
+    try:
+        for num in range(0, x):
+            print(my_list[num], end="")
+            i += 1
+    except IndexError:
+        pass
+    finally:
+        print('')
+        return(num)
