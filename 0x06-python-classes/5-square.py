d3d4e3f6dec9a46e3cf3b5660df9bd5009d0b3ca
sergioq7777 <sergioq.7777@gmail.com>
2020-01-09 23:31:49 -0500

    FIXX

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+class Square:
+    """
+    Class to find an square area
+    """
+    def __init__(self, size=0):
+        """
+        This is the constructor
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Action to obtain the atribute value
+        """
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        return(self.__size**2)
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        for x in range(self.size):
+            print('#'*self.size)
