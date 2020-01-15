cb4d187184cfd340a8adbca7c593a85773eddfa9
sergioq7777 <sergioq.7777@gmail.com>
2020-01-15 17:29:53 -0500

    Space pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 
-# Add two values "int/float" and return a addition.# args = int = add
-
-
 def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
