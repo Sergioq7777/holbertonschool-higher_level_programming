b4e5264e8f990aeb533f434ca912f583c5e4dc3f
sergioq7777 <sergioq.7777@gmail.com>
2020-01-14 22:25:18 -0500

    @

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,1 +1,52 @@
 #!/usr/bin/python3
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+            self.height = height
+            self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        t = ''
+        if self.__width == 0 or self.__height == 0:
+            return t
+        for c in range(self.__height):
+            for i in range(self.__width):
+                if i == self.__width - 1 and c != self.__height - 1:
+                    t += "#\n"
+                else:
+                    t += "#"
+        return t
