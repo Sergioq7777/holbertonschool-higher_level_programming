a8ff92ddd6af8dd004f6f39f6f11068f7e38b970
sergioq7777 <sergioq.7777@gmail.com>
2020-01-15 20:12:17 -0500

    Fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,52 +1,74 @@
 #!/usr/bin/python3
+"""3-rectangle class
+"""
+
+
 class Rectangle:
-
+    """method to config
+    """
     def __init__(self, width=0, height=0):
-            self.height = height
-            self.width = width
+        self.width = width
+        self.height = height
 
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
-
+    """get width
+    """
     @property
     def width(self):
         return self.__width
 
+    """config width
+    """
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
+        if type(value) == int and value >= 0:
             self.__width = value
+        elif type(value) != int:
+            raise NameError("width must be an integer")
+        elif value < 0:
+            raise NameError("width must be >= 0")
 
+    """get heigth
+    """
+    @property
+    def height(self):
+        return self.__height
+
+    """config height
+    """
+    @height.setter
+    def height(self, value):
+        if type(value) == int and value >= 0:
+            self.__height = value
+        elif type(value) != int:
+            raise NameError("height must be an integer")
+        elif value < 0:
+            raise NameError("height must be >= 0")
+
+    """get area
+    """
     def area(self):
-        return (self.__width * self.__height)
+        return self.__height*self.__width
 
+    """get perimeter
+    """
     def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return ((self.__width * 2) + (self.__height * 2))
+        if self.__height == 0 or self.__width == 0:
+            self.__perimeter = 0
+        else:
+            self.__perimeter = 2 * (self.__height + self.__width)
+        return self.__perimeter
 
+    """print rectangle
+    """
     def __str__(self):
-        t = ''
-        if self.__width == 0 or self.__height == 0:
-            return t
-        for c in range(self.__height):
-            for i in range(self.__width):
-                if i == self.__width - 1 and c != self.__height - 1:
-                    t += "#\n"
-                else:
-                    t += "#"
-        return t
+        square = []
+        if self.__width and self.__height:
+            for a in range(0, self.__height):
+                for b in range(self.__width):
+                    square.append("#")
+                if b <= self.__width:
+                    square.append('\n')
+            return "".join(square[:-1])
+        else:
+            square = ''
+            return square
