f90a31405501263a20893995a2e8a81a245eda73
sergioq7777 <sergioq.7777@gmail.com>
2020-01-15 20:14:15 -0500

    Fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,74 +1,52 @@
 #!/usr/bin/python3
-"""3-rectangle class
-"""
-
-
 class Rectangle:
-    """method to config
-    """
+
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+            self.height = height
+            self.width = width
 
-    """get width
-    """
-    @property
-    def width(self):
-        return self.__width
-
-    """config width
-    """
-    @width.setter
-    def width(self, value):
-        if type(value) == int and value >= 0:
-            self.__width = value
-        elif type(value) != int:
-            raise NameError("width must be an integer")
-        elif value < 0:
-            raise NameError("width must be >= 0")
-
-    """get heigth
-    """
     @property
     def height(self):
         return self.__height
 
-    """config height
-    """
     @height.setter
     def height(self, value):
-        if type(value) == int and value >= 0:
-            self.__height = value
-        elif type(value) != int:
-            raise NameError("height must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise NameError("height must be >= 0")
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
-    """get area
-    """
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
     def area(self):
-        return self.__height*self.__width
+        return (self.__width * self.__height)
 
-    """get perimeter
-    """
     def perimeter(self):
-        if self.__height == 0 or self.__width == 0:
-            self.__perimeter = 0
-        else:
-            self.__perimeter = 2 * (self.__height + self.__width)
-        return self.__perimeter
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((self.__width * 2) + (self.__height * 2))
 
-    """print rectangle
-    """
     def __str__(self):
-        square = []
-        if self.__width and self.__height:
-            for a in range(0, self.__height):
-                for b in range(self.__width):
-                    square.append("#")
-                if b <= self.__width:
-                    square.append('\n')
-            return "".join(square[:-1])
-        else:
-            square = ''
-            return square
+        s = ''
+        if self.__width == 0 or self.__height == 0:
+            return s
+        for c in range(self.__height):
+            for f in range(self.__width):
+                if f == self.__width - 1 and c != self.__height - 1:
+                    s += "#\n"
+                else:
+                    s += "#"
+        return s
