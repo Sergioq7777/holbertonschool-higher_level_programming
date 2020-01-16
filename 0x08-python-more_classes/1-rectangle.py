17bd2abf9659fc194587fe43be2417bb0a8cf38b
sergioq7777 <sergioq.7777@gmail.com>
2020-01-15 20:10:14 -0500

    PEp

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,9 +5,11 @@ class Rectangle:
     def __init__(self, width=0, height=0):
             self.height = height
             self.width = width
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -20,6 +22,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
