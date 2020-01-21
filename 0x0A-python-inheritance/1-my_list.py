059fb96802c0e8522ab0e65c38af9453981fa404
sergioq7777 <55101797+Sergioq7777@users.noreply.github.com>
2020-01-21 18:26:13 -0500

    Delete 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" ADD and Sort elemnts on a list """
-
-
-class MyList(list):
-    def print_sorted(self):
-        list_a = self[:]
-        list_a.sort()
-        print(list_a)
