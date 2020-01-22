5b42b69cf065d917d864e618ccdfab4c2df838c9
sergioq7777 <sergioq.7777@gmail.com>
2020-01-21 21:29:19 -0500

    Squer

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+
+        self.__size = size
+        self.integer_validator("size", self.__size)
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+
+        return (super().area())
+
+    def __str__(self):
+
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
