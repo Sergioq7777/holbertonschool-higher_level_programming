8afe3a12c63a15e9629036d37615dc6bc85fe6eb
sergioq7777 <sergioq.7777@gmail.com>
2020-01-22 23:04:39 -0500

    ADD

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+
+    if n <= 0:
+        return []
+
+    pas_r = [[1]]
+    if n > 1:
+        pas_r.append([1, 1])
+        for ind in range(3, n + 1):
+            pas_r.append([1] + list(map(
+                lambda i: pas_r[ind - 2][i] + pas_r[ind - 2][i + 1], range(
+                    len(pas_r[ind - 2]) - 1))) + [1])
+    return pas_r
