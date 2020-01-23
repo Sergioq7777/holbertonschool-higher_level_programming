71b22bc7f880436473e5b74ab9dd729424a14bc5
sergioq7777 <sergioq.7777@gmail.com>
2020-01-22 23:07:49 -0500

    Fix

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
+'''
+Write a script that adds all arguments to a Python list
+'''
+from sys import argv
 
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-def main():
-    list_args = []
-    try:
-        if (len(sys.argv) - 1) >= 1:
-            list = load_from_json_file("add_item.json")
-            for data in list:
-                list_args.append(data)
-    except:
-            pass
+try:
+    list2add = load_from_json_file('add_item.json')
+except:
+    list2add = []
 
-    for arg in sys.argv[1:]:
-        if (len(sys.argv) - 1) >= 1:
-            list_args.append(str(arg))
-        else:
-            list_args.append("")
-
-    save_to_json_file(list_args, "add_item.json")
-
-if __name__ == '__main__':
-    import sys
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('8-load_from_json_file').\
-        load_from_json_file
-    main()
+for arg in argv[1:]:
+    list2add.append(arg)
+save_to_json_file(list2add, 'add_item.json')
