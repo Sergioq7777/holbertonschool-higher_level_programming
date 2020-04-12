29d0cf40df1f28dc0eb9ebe6111b08de7ac2e6b1
sergioq7777 <sergioq.7777@gmail..com>
2020-04-12 14:56:42 -0500

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Function to find a peak """
+
+
+def findPeakUtil(arr, low, high):
+    """ Using divide and conque algorithm"""
+    if not arr:
+        return None
+
+    mid = int((high + low)/2)
+
+    if ((mid == 0 or arr[mid - 1] <= arr[mid]) and (
+            mid == len(arr) - 1 or arr[mid + 1] <= arr[mid])):
+        return arr[mid]
+
+    elif (mid > 0 and arr[mid - 1] > arr[mid]):
+        return findPeakUtil(arr, low, (mid - 1))
+
+    else:
+        return findPeakUtil(arr, (mid + 1), high)
+
+
+def find_peak(list_of_integers):
+    """Find peak method"""
+    if not list_of_integers:
+        return None
+
+    Max = len(list_of_integers) - 1
+    Min = 0
+    return findPeakUtil(list_of_integers, Min, Max)
+    
