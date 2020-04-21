fea78fa3a20331f62315271dc842f9a74acc2119
sergioq7777 <sergioq.7777@gmail..com>
2020-04-20 22:10:37 -0500

    irlib fr first

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Check status"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        dic = r.json()
+        if not dic:
+            print("No result")
+        else:
+            print("[{}] {}".format(dic.get("id"), dic.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
