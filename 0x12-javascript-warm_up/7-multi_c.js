2227edbd938b3507e8814a73de9e9b23b7dde4aa
sergioq7777 <sergioq.7777@gmail..com>
2020-05-05 23:53:56 -0500

    multi c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (parseInt(process.argv.slice(2)[0])) {
+    if (parseInt(process.argv.slice(2)[0]) >= 1) {
+      const myVar = 'C is fun';
+      let i = parseInt(process.argv.slice(2)[0]);
+      while (i) {
+        console.log(myVar);
+        i -= 1;
+      }
+    }
+  } else {
+    console.log('Missing number of occurrences');
+  }
