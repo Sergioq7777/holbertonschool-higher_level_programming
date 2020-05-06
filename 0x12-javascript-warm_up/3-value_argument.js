d7b12d2f17629106832571a45bd76678074f77c9
sergioq7777 <sergioq.7777@gmail..com>
2020-05-06 00:04:47 -0500

    fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
-
 #!/usr/bin/node
-if (process.argv.slice(2)[0]) {
-  console.log(process.argv.slice(2)[0]);
-} else {
-  console.log('No argument');
-}
+
+if (process.argv[2]) {
+    console.log(process.argv[2]);
+  } else {
+    console.log('No argument');
+  }
