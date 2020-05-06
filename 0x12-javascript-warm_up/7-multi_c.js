a387bd73ef01dc58cba815ef75e13f15199e836b
sergioq7777 <sergioq.7777@gmail..com>
2020-05-06 00:24:42 -0500

    add

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-if (parseInt(process.argv.slice(2)[0])) {
-    if (parseInt(process.argv.slice(2)[0]) >= 1) {
-      const myVar = 'C is fun';
-      let i = parseInt(process.argv.slice(2)[0]);
-      while (i) {
-        console.log(myVar);
-        i -= 1;
-      }
-    }
-  } else {
-    console.log('Missing number of occurrences');
+const num = process.argv[2];
+if (num) {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurrences');
+}
