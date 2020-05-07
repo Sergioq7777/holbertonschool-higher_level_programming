c164b06f2be424310970873f4d02fd3180a9cd20
sergioq7777 <sergioq.7777@gmail..com>
2020-05-07 14:21:21 -0500

    chmod u+x

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.callMeMoby = function (a, funct) {
-    let str = '';
-    for (let ind = 0; ind < a; ind++) {
-      str += funct();
-    }
-    return (str);
-  };
+  let str = '';
+  for (let ind = 0; ind < a; ind++) {
+    str += funct();
+  }
+  return (str);
+};
