c40f709d7d4745b7f4ab4c120e93b6f076e30bd7
sergioq7777 <sergioq.7777@gmail.com>
2020-05-15 04:02:39 +0000

    [feature] all in

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,6 @@
+Bytes
+const doc = window.$;
+doc('#toggle_header').click(function () {
+	doc('header').toggleClass('red');
+	doc('header').toggleClass('green');
+});
---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,5 @@
+
+const doc = window.$;
+doc.get('https://swapi.co/api/people/5/?format=json', function (data) {
+	  $('#character').text(data.name);
+});
